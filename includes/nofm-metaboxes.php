9240c8cd7968ae68f1056ee96007b563d3248d31
hela <luah613@gmail.com>
2019-03-18 13:26:58 -0600

    save metadata with plugin

---
+++ includes/nofm-metaboxes.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * Register the meta key to rest api
+*/
+function nofm_register_meta(){
+	register_meta('post','_id_youtube',array('type'=>'string','single'=>true,'show_in_rest'=>true));
+}
+add_action('init', 'nofm_register_meta');
+
+/*
+ * This method will add our custom route to Wordpress REST API
+ * /nofm/v2/update-meta/
+*/
+function nofm_api_posts_meta_field(){
+	register_rest_route(
+		'nofm/v2',
+		'/update-meta',
+		array(
+			'methods'=>'POST',
+			'callback'=>'nofm_update_meta_callback',
+			'args'=>array('id'=>array('sanitize_callback'=>'absint'))
+		)
+	);
+}
+add_action('rest_api_init', 'nofm_api_posts_meta_field');
+
+function nofm_update_meta_callback($data){
+	return update_post_meta($data['id'], $data['key'], $data['value']);
+}
--- src/sidebar.js
+++ src/sidebar.js
@@ -8,3 +8,93 @@ const {Component,Fragment} = wp.element;
 const {withSelect} = wp.data;
 const {registerPlugin} = wp.plugins;
 const {addAction} = wp.hooks;
+
+class NofmMetadata extends Component{
+	constructor(props){
+		super(props);
+
+		this.handleInputValue = this.handleInputValue.bind(this);
+
+		this.state={
+			key: '_id_youtube',
+			value: ''
+		}
+
+		wp.apiFetch({
+			path: `/wp/v2/posts/${this.props.postId}`, 
+			method: 'GET'
+		}).then(data=>{
+			// console.log(data.meta);
+			this.setState({
+				key:'_id_youtube',
+				value: data.meta._id_youtube
+			});
+			return data;
+		}).catch(error=>console.error(error));
+
+	}//end constructor
+
+
+	static getDerivedStateFromProps(nextProps, state){
+
+		if((nextProps.isPublishing||nextProps.isSaving) && !nextProps.isAutoSaving){
+			wp.apiRequest({
+				path: `/nofm/v2/update-meta?id=${nextProps.postId}`,
+				method: 'POST',
+				data: state
+			}).then(
+				(data)=>{
+					return data;
+			},(err)=>{
+				return err;
+			});
+		}
+
+	}//end get derived state from props
+
+
+	render(){
+		// console.log(this.state);
+		return(
+			<Fragment>
+				<PluginSidebarMoreMenuItem target="nofm-metadata-sidebar">
+					{ __( 'NoFm Metadata' ) }
+				</PluginSidebarMoreMenuItem>
+				<PluginSidebar name="nofm-metadata-sidebar" title={ __( 'Nofm Metadata' ) }>
+					<PanelBody>
+						<TextControl
+							label={ __( 'ID de youtube' ) }
+							value={this.state.value}
+							onChange={this.handleInputValue}
+						/>
+					</PanelBody>
+				</PluginSidebar>
+			</Fragment>
+		)
+	}//end render
+
+	handleInputValue(value){
+		console.log(value);
+		this.setState({
+			key: '_id_youtube',
+			value: value
+		})
+	}
+
+}
+
+const HOC = withSelect((select, {forceIsSaving})=>{
+	const { getCurrentPostId, isSavingPost, isPublishingPost, isAutosavingPost, getEditedPostAttribute, isTyping, getPermalink} = select('core/editor');
+	return{
+		postId: getCurrentPostId(),
+		isSaving: forceIsSaving || isSavingPost(),
+		isAutoSaving: isAutosavingPost(),
+		isPublishing: isPublishingPost(),
+		isTyping: isTyping(),
+	}
+})(NofmMetadata);
+
+registerPlugin('nofm-metadata', {
+	icon: 'editor-spellcheck',
+	render: HOC
+});
