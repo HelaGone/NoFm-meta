e2d0a068927fb4d654e0ed4e7778c92c7cc6bc12
Holkan <luah613@gmail.com>
2019-03-19 18:43:20 -0600

    minor fix

--- src/sidebar.js
+++ src/sidebar.js
@@ -262,7 +262,7 @@ class NofmMetadata extends Component{
 
 	handleInputValue(event){
 		const target = event.target;
-		const value = (target.type==='checkbox') ? target.checked : target.value;
+		const value = target.value;
 		const name = target.name;
 
 		this.setState({
